2837ba1658816f71bacc8b2aa787f10b12290ac6
BTheofil <btheofil7@gmail.com>
2026-05-22 16:24:17 +0200

    new version

--- server/src/main/kotlin/hu/tb/routing/Swagger.kt
+++ server/src/main/kotlin/hu/tb/routing/Swagger.kt
@@ -1,12 +1,14 @@
 package hu.tb.routing
 
 import io.ktor.http.*
+import io.ktor.openapi.OpenApiInfo
 import io.ktor.server.plugins.swagger.*
 import io.ktor.server.routing.*
 import io.ktor.server.routing.openapi.*
 
 fun Route.swaggerApi() {
     swaggerUI("/swagger") {
+        info = OpenApiInfo(title = "Endpoints", version = "1.1.0")
         source = OpenApiDocSource.Routing(ContentType.Application.Json) {
             routingRoot.descendants()
         }
