7a4bc359a59a3c88aefcbfd6f321e58a42f19c98
BTheofil <btheofil7@gmail.com>
2026-06-10 19:25:16 +0200

    fixed websocket connection

--- client/network/src/main/java/hu/tb/network/message/MessageRepository.kt
+++ client/network/src/main/java/hu/tb/network/message/MessageRepository.kt
@@ -5,11 +5,9 @@ import hu.tb.network.message.model.response.Message
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.websocket.receiveDeserialized
 import io.ktor.client.plugins.websocket.webSocket
-import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
 import io.ktor.http.HttpHeaders
-import io.ktor.websocket.Frame
 import kotlinx.coroutines.flow.first
 
 class MessageRepository(
@@ -20,10 +18,10 @@ class MessageRepository(
         try {
             val token = userDatastore.userdataFlow().first().token
             val session = client.webSocket(
-                //urlString = "ws://[2a01:4f9:c014:f7e9::1]:8080/groupConnect",
+                path = "/groupConnect",
                 request = {
                     parameter(CLIENT_PARAMETER, groupId)
-                    header(HttpHeaders.Authorization, token)
+                    header(HttpHeaders.Authorization, "Bearer $token")
                 },
                 block = {
                     println("hello")
