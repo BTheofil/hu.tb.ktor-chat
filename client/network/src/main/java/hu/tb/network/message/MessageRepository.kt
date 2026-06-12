43fb31e0b1d4ffaac429f5ed28451cfbb6be818d
BTheofil <btheofil7@gmail.com>
2026-06-12 22:44:35 +0200

    fixed websocket connection

--- client/network/src/main/java/hu/tb/network/message/MessageRepository.kt
+++ client/network/src/main/java/hu/tb/network/message/MessageRepository.kt
@@ -3,36 +3,46 @@ package hu.tb.network.message
 import hu.tb.datastore.UserDatastoreRepository
 import hu.tb.network.message.model.response.Message
 import io.ktor.client.HttpClient
-import io.ktor.client.plugins.websocket.receiveDeserialized
-import io.ktor.client.plugins.websocket.webSocket
+import io.ktor.client.plugins.websocket.webSocketSession
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
 import io.ktor.http.HttpHeaders
+import io.ktor.websocket.Frame
+import io.ktor.websocket.WebSocketSession
+import io.ktor.websocket.readText
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.isActive
+import kotlinx.serialization.json.Json
 
 class MessageRepository(
     private val client: HttpClient,
     private val userDatastore: UserDatastoreRepository
 ) {
-    suspend fun sendMessage(groupId: Long) {
+    private var session: WebSocketSession? = null
+
+    suspend fun sendMessage(groupId: Long): Flow<Message>? {
         try {
             val token = userDatastore.userdataFlow().first().token
-            val session = client.webSocket(
+            session = client.webSocketSession(
+                port = 8080,
                 path = "/groupConnect",
-                request = {
+                block = {
                     parameter(CLIENT_PARAMETER, groupId)
                     header(HttpHeaders.Authorization, "Bearer $token")
-                },
-                block = {
-                    println("hello")
-                    for (frame in incoming) {
-                        val message = receiveDeserialized<Message>()
-                        println(message)
-                    }
                 }
             )
+            
+            return session?.incoming
+                ?.consumeAsFlow()
+                ?.filterIsInstance<Frame.Text>()
+                ?.map { Json.decodeFromString<Message>(it.readText()) }
         } catch (e: Exception) {
             e.printStackTrace()
+            return null
         }
     }
 }
